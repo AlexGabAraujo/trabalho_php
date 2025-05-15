957f4eeb0c4d1d2e82ce869a3f480c90c4eaf685
lily-mieruko-chan <dudapereiraotaku@gmail.com>
2025-05-14 21:34:36 -0300

    Add files via upload

---
+++ trabalho_php/cadastro.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "TRABALHOPHP";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $cpf = $_POST['CPF'];
+    $email = $_POST['email'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO usuario (nome, cpf, email, senha) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $nome, $cpf, $email, $senha);
+
+    if ($stmt->execute()) {
+        echo "<p style='color: green;'>Cadastro realizado com sucesso!</p>";
+    } else {
+        echo "<p style='color: red;'>Erro ao cadastrar: " . $stmt->error . "</p>";
+    }
+
+    $stmt->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Cadastro</title>
+
+  
+</head>
+<body>
+
+</body>
+</html>
+
+
+
